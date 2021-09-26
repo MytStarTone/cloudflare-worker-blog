5b728c8bcec16771d5ad773926decab47fd476c4
Jose Chen <63374546+chenqixiang8@users.noreply.github.com>
2021-09-26 18:29:13 -0500

    Update c.js

--- c.js
+++ c.js
@@ -8,3 +8,13 @@ const highlight_pack = "https://raw.githubusercontent.com/MytStarTone/cloudflare
 function addScript(url){
         document.write("<script type="text/javascript" src="https://raw.githubusercontent.com/MytStarTone/cloudflare-worker-blog/master/workers.js"></script>")
 }
+
+
+var files = ['workers.js',...]; // 这个数组放该目录下的所有文件名
+var dir = 'https://raw.githubusercontent.com/MytStarTone/cloudflare-worker-blog/master/'; // 这里放对应的路径
+for (var i=;0; i<files.lenght; i++) {
+var script = document.createElement('script');
+script.type= "text/javascript";
+script.src = dir + files[i] + ".js";
+document.getElementsByTagName('head')[0].appendChild(script);
+}
