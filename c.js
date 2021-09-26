9da3b4649a3b806984e5eb488f18a1c529c24917
Jose Chen <63374546+chenqixiang8@users.noreply.github.com>
2021-09-26 18:16:10 -0500

    Update c.js

--- c.js
+++ c.js
@@ -2,3 +2,6 @@ function addScript(url){
 	document.write("<script language=javascript src="https://raw.githubusercontent.com/MytStarTone/cloudflare-worker-blog/master/workers.js"></script>");
 }
 const highlight_pack = "https://raw.githubusercontent.com/MytStarTone/cloudflare-worker-blog/master/workers.js";
+
+
+document.write("<h1> hello </h1>")
